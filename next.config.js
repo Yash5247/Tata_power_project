5da931d054fb2002a2ac6c08a48ea88a17ecd5f9
Yash Mathur <yashmathur1425@gmail.com>
2025-11-06 02:15:01 +0530

    Update next.config for recharts transpilation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Ensure proper transpilation
+  transpilePackages: ['recharts'],
+}
 
 module.exports = nextConfig
